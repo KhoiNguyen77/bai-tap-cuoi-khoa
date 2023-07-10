633ca6753c4c290cab9f490b48c79116a768d9ae
thanh trinh <trinhphuthanh95@gmail.com>
2023-07-10 08:44:39 +0700

    thanh commit

--- src/Template/HomeTemplate.tsx
+++ src/Template/HomeTemplate.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import Header from '../Layout/Header'
+import Footer from '../Layout/Footer'
+import { Outlet } from 'react-router-dom'
 
 type Props = {}
 
 const HomeTemplate = (props: Props) => {
     return (
-        <div className='font-bold'>HomeTemplate</div>
+        <div >
+            <div style={{ minHeight: '80vh' }}>
+                <Header></Header>
+            </div>
+            <div className="content" >
+                <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
+        </div>
     )
 }
 
